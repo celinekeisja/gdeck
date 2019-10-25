4fe3ef268b759b71613d6c1af7af72c812640c5d
Celine Keisja <celinekeisja@gmail.com>
2019-10-25 14:13:00 +0800

    Update tests

--- GDeck.py
+++ GDeck.py
@@ -1,22 +1,42 @@
 
 class Card:
+    """ Card class that has user-defined suit and rank. """
     def __init__(self, suit, rank):
+        """ Define a Card object that has a suits and ranks. """
         self.suit = suit
         self.rank = rank
 
     def __repr__(self):
+        """ Return the suit and rank of the Card  """
         return f'{self.rank} of {self.suit}'
 
 
 class Deck:
+    """ Deck class that contains objects from the Card class. """
     suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
     ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
     def __init__(self):
-        self.cards = [Card(suit, rank) for suit in Deck.suits for rank in Deck.ranks]
+        """ Instantiate a deck that has card objects. """
+        self.cards = [f'{Card(suit, rank)}' for suit in Deck.suits for rank in Deck.ranks]
+        self.ctr = 0
 
-    def __getitem__(self, next):
-        return self.cards[next]
+    def __next__(self):
+        """ Identify the next card in the deck in order. """
+        if self.ctr >= len(self.cards):
+            raise StopIteration
+        card = self.cards[self.ctr]
+        self.ctr += 1
+        return card
+
+    def __getitem__(self, index):
+        """ Get the identity of the card in the defined index. """
+        return self.cards[index]
 
     def __len__(self):
+        """ Get the length of the deck. """
         return len(self.cards)
+
+    def __repr__(self):
+        """ Identify that the object is a 'Deck of cards'. """
+        return str(self.cards)
---
+++ tests/test_GDeck.py
@@ -0,0 +1,22 @@
+from GDeck import *
+
+test_suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
+test_ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+
+def test_card():
+    test_card_class = Card('test_suit', 'test_rank')
+    assert test_card_class.suit == 'test_suit'
+    assert test_card_class.rank == 'test_rank'
+    assert f'{test_card_class}' == 'test_rank of test_suit'
+
+
+def test_deck():
+    assert str(Deck()) == str([f'{rank} of {suit}' for suit in Deck.suits for rank in Deck.ranks])
+    assert Deck.ranks == test_ranks
+    assert Deck.suits == test_suits
+    assert f'{Deck()[0]}' == 'Ace of Diamonds'
+    assert f'{next(Deck())}' == f'{Deck.ranks[0]} of {Deck.suits[0]}'
+    assert len(Deck()) == 52
+
+
